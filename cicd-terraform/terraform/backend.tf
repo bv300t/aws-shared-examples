5667683ebfeb2b097f2caef1037ca2a4b8855730
bv300t <69912607+bv300t@users.noreply.github.com>
2025-04-05 09:32:16 -0400

    Create backend.tf
    create initial backend.tf

---
+++ cicd-terraform/terraform/backend.tf
@@ -0,0 +1,8 @@
+  required_version = ">= 0.13"
+  backend "s3" {
+    profile = "demo"
+    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    bucket  = "<bucket-name-placeholder>"
+  }
+}
