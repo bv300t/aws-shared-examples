55cf9a8818e810cb4ec158a48d397c62ad554714
bv300t <69912607+bv300t@users.noreply.github.com>
2025-04-05 10:08:35 -0400

    Update backend.tf
    remove local demo profile

--- cicd-terraform/terraform/backend.tf
+++ cicd-terraform/terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   required_version = ">= 0.13"
   backend "s3" {
-    profile = "demo"
     region  = "us-east-1"
     key     = "terraform.tfstate"
     bucket  = "<bucket-name-placeholder>"
